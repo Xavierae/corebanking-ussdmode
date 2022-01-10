5dd20e7311c6f42a2ddcb56fa09e641280a6385e
Xaviera Ecclésiaste <44640299+Xavierae@users.noreply.github.com>
2022-01-10 16:12:55 +0100

    new menu

--- index.php
+++ index.php
@@ -7,27 +7,27 @@ $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON Welcome to Xearth Core Banking \n";
-    $response .= "1. Account Detail \n";
-    $response .= "2. Check Balance";
-    /* $response .= "3. Register \n";
-    $response .= "4. Send Money";
-    $response .= "5. Check Balance \n";
-    $response .= "6. Withdraw Cash "; */
+    $response  = "CON What would you want to check \n";
+    $response .= "1. Register \n";
+    $response .= "2. Account Information \n";
+    $response .= "1. Send Money \n";
+    $response .= "2. Check Balance \n";
+    $response .= "1. Withdraw Cash \n";
+    $response .= "6. Quit \n";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Your personal information \n";
-    $response .= "1. Audited \n";
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "Your total balance is ".$phoneNumber;
+    $response = "END Your phone number is ".$phoneNumber;
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "XAVIERASSI001";
+    $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your account number is ".$accountNumber;
