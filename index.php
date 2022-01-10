3491c37d95596f74a44342b0de3dd789a416f101
Xaviera Ecclésiaste <44640299+Xavierae@users.noreply.github.com>
2022-01-10 16:29:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
+    $response  = "CON Welcome on DigitPay code USSD by XEARTH \n";
     $response .= "1. Register \n";
     $response .= "2. Account Information \n";
-    $response .= "1. Send Money \n";
-    $response .= "2. Check Balance \n";
-    $response .= "1. Withdraw Cash \n";
+    $response .= "3. Send Money \n";
+    $response .= "4. Check Balance \n";
+    $response .= "5. Withdraw Cash \n";
     $response .= "6. Quit \n";
 
 } else if ($text == "1") {
@@ -25,14 +25,34 @@ if ($text == "") {
     // This is a terminal request. Note how we start the response with END
     $response = "END Your phone number is ".$phoneNumber;
 
-} else if($text == "1*1") { 
+/* } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
     $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your account number is ".$accountNumber;
 
-}
+} */
+
+} else if ($text == "3") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if ($text == "4") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if ($text == "5") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if ($text == "6") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "End of session ";
 
 // Echo the response back to the API
 header('Content-type: text/plain');
